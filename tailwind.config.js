b8e1ab6c6d98907454360b3c9a06bb4ba1c863d0
HanVbear <ryoitenkaii@gmail.com>
2025-12-09 19:31:58 +0700

    add some starting code and set up config native wind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#EEDBBF',
+        scarlet: '#ab3130',
+        deepred: '#67191f',
+        midnight: '#121212',
+        golden: '#b47a21'
+      },
+      fontFamily: {
+        "quicksand": ["Quicksand-Regular", "sans-serif"],
+        "quicksand-bold": ["Quicksand-Bold", "sans-serif"],
+        "quicksand-semibold": ["Quicksand-SemiBold", "sans-serif"],
+        "quicksand-light": ["Quicksand-Light", "sans-serif"],
+        "quicksand-medium": ["Quicksand-Medium", "sans-serif"],
+
+        "cinzel-bold": ["Cinzel-Bold", "sans-serif"],
+        "cinzel-medium": ["Cinzel-Medium", "sans-serif"],
+        "cinzel": ["Cinzel-Regular", "sans-serif"],
+
+        "great": ["GreatVibes", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
