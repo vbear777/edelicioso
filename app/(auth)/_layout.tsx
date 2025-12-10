9ca9233eace27b76f384c4861d9439c4b454d841
HanVbear <ryoitenkaii@gmail.com>
2025-12-10 16:51:47 +0700

    set up appwrite and made initial code for auth screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground } from "react-native";
 import React from 'react';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Slot } from "expo-router";
+import { images } from "@/constants";
 
 export default function AuthLayout() {
     return (
-        <SafeAreaView>
-            <Text> Auth Layout </Text>
+        <KeyboardAvoidingView behavior={Platform.OS === 'android' ? 'padding' : 'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch" />
+                </View>
+            </ScrollView>
             <Slot />
-        </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.hans.edelicioso",
+    databaseId: '69393ebe002ee4949eb4',
+    userCollectionId: 'user',
+}
