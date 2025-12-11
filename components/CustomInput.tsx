1a434f86dc2af284a63166b1f3c0d7cf1a0799f1
HanVbear <ryoitenkaii@gmail.com>
2025-12-11 07:47:48 +0700

    adjust customInput component

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,5 +1,8 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { CustomInputProps } from "@/type";
+import { useState } from "react";
+import cn from "clsx";
+
 
 const CustomInput = ({ 
     placeholder = 'Enter text', 
@@ -9,9 +12,25 @@ const CustomInput = ({
     secureTextEntry = false,
     keyboardType = "default"
 }: CustomInputProps) => {
+    const [isFocused, setIsFocused] = useState(false);
+
     return (
         <View className="w-full">
             <Text className="text-base text-start w-full font-quicksand-medium text-gray-500 pl-2">{label}</Text>
+
+            <TextInput 
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={value}
+                onChangeText={onChangeText}
+                secureTextEntry={secureTextEntry}
+                keyboardType={keyboardType}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                placeholder={placeholder}
+                placeholderTextColor="#888"
+                className={cn('input', isFocused ? 'border-golden' : 'border-gray-300')}
+            />
         </View>
     )
 }
