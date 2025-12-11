b9a12337c011462314bb273704145c31b5cb9ccf
HanVbear <ryoitenkaii@gmail.com>
2025-12-11 08:21:41 +0700

    add CustomButton component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,11 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 import React from 'react';
+import { CustomButtonProps } from "@/type";
+import cn from "clsx";
 
-const CustomButton = () => {
+const CustomButton = ({
+    onPress,
+    title="Click Me",
+    style,
+    textStyle,
+    leftIcon,
+    isLoading = false
+}: CustomButtonProps) => {
     return (
-        <View>
-            <Text>CustomButton</Text>
-        </View>
+        <TouchableOpacity className={cn('bg-golden rounded-full p-3 w-full flex flex-row justify-center', style)} onPress={onPress}>
+            {leftIcon}
+
+            <View className="flex items-center justify-center flex-row">
+                {isLoading ? (
+                    <ActivityIndicator size="small" color="blue" />
+                ): (
+                    <Text className={cn('text-white text-base font-quicksand-semibold', textStyle)}>
+                        {title}
+                    </Text>
+                )}
+            </View>
+        </TouchableOpacity>
     )
 }
 
