9e92551a13a6d995f69165e8b6e15ff6da6eac96
HanVbear <ryoitenkaii@gmail.com>
2025-12-11 22:04:00 +0700

    adjust appwrite setting

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -7,45 +7,67 @@ export const appwriteConfig = {
     platform: "com.hans.edelicioso",
     databaseId: '69393ebe002ee4949eb4',
     userCollectionId: 'user',
-}
+};
 
 export const client = new Client();
 
 client
     .setEndpoint(appwriteConfig.endpoint)
     .setProject(appwriteConfig.projectId)
-    .setPlatform(appwriteConfig.platform)
+    .setPlatform(appwriteConfig.platform);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 const avatars = new Avatars(client);
 
-export const createUser = async ({ email, password, name}: CreateUserParams) => {
-    try {
-        const newAccount = await account.create({userId: ID.unique(), email, password, name})
-        if (!newAccount) throw Error;
 
-        await signIn({ email, password});
+//REGISTER USER
+export const createUser = async ({ email, password, name }: CreateUserParams) => {
+    try {
+        const newAccount = await account.create({
+            userId: ID.unique(),
+            email,
+            password,
+            name,
+        });
+
+        if (!newAccount) throw new Error("Failed to create account.");
+
+        // Auto-login after register
+        await signIn({ email, password });
 
         const avatarUrl = avatars.getInitialsURL(name);
 
+        // make user document for database
         return await databases.createDocument({
             databaseId: appwriteConfig.databaseId,
             collectionId: appwriteConfig.userCollectionId,
             documentId: ID.unique(),
-            data: { email, name, accountId: newAccount.$id, avatar: avatarUrl
-            }}
-        );
-    } catch(e) {
-        throw new Error(e as string);
+            data: {
+                email,
+                name,
+                accountId: newAccount.$id,
+                avatar: avatarUrl,
+            },
+        });
+    } catch (err: any) {
+        // show erro from appwrite 
+        throw new Error(err?.message || "Failed to create user.");
     }
-}
+};
 
+
+//SIGN IN SCREEN
 export const signIn = async ({ email, password }: SignInParams) => {
     try {
-        const session = await account.createEmailPasswordSession({email, password});
-    } catch (e) {
-        throw new Error(e as string);
+        const session = await account.createEmailPasswordSession({
+            email,
+            password,
+        });
+
+        return session;
+    } catch (err: any) {
+        throw new Error(err?.message || "Failed to sign in.");
     }
-    
-}
+};
+
