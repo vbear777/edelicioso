2b7c3dfb460175325bcaa61721c830125a6fd07b
HanVbear <ryoitenkaii@gmail.com>
2025-12-11 13:41:34 +0700

    add state to Sign In

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,28 +1,50 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import { View, Text, Button, Alert } from "react-native";
+import { useState } from "react";
+import { Link, router } from "expo-router";
 import CustomInput from "@/components/CustomInput";
 import CustomButton from "@/components/CustomButton";
 
 const SignIn = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [form, setForm] = useState({ email: '', password: ''});
+
+    const submit = async () => {
+        if(!form.email || !form.password) Alert.alert('Error', 'Please enter valid email address & password');
+
+        setIsSubmitting(true)
+
+        try {
+            //Call appwrite Sign in function
+
+            Alert.alert('Success', 'User signed in successfully.');
+            router.replace('/');
+        } catch(error: any) {
+            Alert.alert('Error', error.message);
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
+    
     return (
         <View className="gap-10 bg-white rounded-lg p-5 mt-5">
             <CustomInput 
                 placeholder="Enter your email"
-                value={''}
-                onChangeText={(text) => {}}
+                value={form.email}
+                onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
                 label="Email"
                 keyboardType="email-address"
             />
             <CustomInput 
                 placeholder="Enter your password"
-                value={''}
-                onChangeText={(text) => {}}
+                value={form.password}
+                onChangeText={(text) => setForm((prev) => ({ ...prev, password: text }))}
                 label="Password"
                 secureTextEntry={true}
             />
             <CustomButton 
                 title="Sign In"
+                isLoading={isSubmitting}
+                onPress={submit}
             />
 
             <View className="flex justify-center mt-5 flex-row gap-2">
