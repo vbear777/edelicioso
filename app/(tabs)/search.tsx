875631530609261e95a8e503130ff1d0cf7fe99d
HanVbear <ryoitenkaii@gmail.com>
2025-12-18 18:49:11 +0700

    adjust search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { getCategories, getMenu } from "@/lib/appwrite";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect } from "react";
+import CartButton from "@/components/CartButton";
 
 const Search = () => {
     const { category, query } = useLocalSearchParams<{query: string; category: string}>()
@@ -23,16 +24,40 @@ const Search = () => {
         refetch({ category, query, limit: 6})
     }, [category, query]);
 
-    console.log("DATA LENGTH:", data?.length);
     return (
         <SafeAreaView className="bg-white h-full ">
-            <FlatList data={data} renderItem={({ item, index }) => {
+            <FlatList 
+                data={data} 
+                renderItem={({ item, index }) => {
                 return (
                     <View className="flex-1 max-w-[48%]">
                         <Text>Menu Card</Text>
                     </View>
                 )                
-            }} />
+                }}
+                keyExtractor={item => item.$id}
+                numColumns={2}
+                columnWrapperClassName="gap-7"
+                contentContainerClassName="gap-7 px-5 pb-32"
+                ListHeaderComponent={() => (
+                        <View className="my-5 gap-5">
+                            <View className="flex items-center justify-between flex-row w-full">
+                                <View className="flex items-start justify-center">
+                                    <Text className="text-3xl font-great text-golden">Search</Text>
+                                    <View className="flex items-start justify-center flex-row gap-x-1 mt-0.5">
+                                        <Text className="text-base font-quicksand-semibold text-midnight">Find your favourite food</Text>
+                                    </View>
+                                </View>
+
+                                <CartButton />
+                            </View>
+                            
+                            <Text>Search Input</Text>
+                            <Text>Filter</Text>
+                        </View>
+                )} 
+                ListEmptyComponent={() => !loading && <Text>No results</Text>}
+            />
         </SafeAreaView>
     )
 }
