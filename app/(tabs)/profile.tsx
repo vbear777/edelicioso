46b9ce618f5b439f6ae711f6a99c896d199b8ed2
HanVbear <ryoitenkaii@gmail.com>
2025-12-20 21:05:18 +0700

    adjust button cart

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>Profile</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,12 +1,13 @@
 import { images } from "@/constants";
 import { useCartStore } from "../store/cart.store";
 import { View, Text, TouchableOpacity, Image } from "react-native";
+import { router } from "expo-router";
 
 const CartButton = () => {
     const { getTotalItems } = useCartStore();
     const totalItems = getTotalItems();
     return (
-        <TouchableOpacity className="flex items-center justify-center size-10 rounded-full bg-midnight" onPress={() => {}}>
+        <TouchableOpacity className="flex items-center justify-center size-10 rounded-full bg-midnight" onPress={() => router.push('/cart')}>
             <Image source={images.bag} className="size-5" resizeMode="contain" />
 
             {totalItems > 0 && (
